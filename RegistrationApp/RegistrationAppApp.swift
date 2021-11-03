b7a954dbd4ed1829a6427ca4b14895079038bd52
Nadzeya Shpakouskaya <shpakovna@gmail.com>
2021-11-03 09:28:01 +0400

    Initial Commit

---
+++ RegistrationApp/RegistrationAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RegistrationAppApp.swift
+//  RegistrationApp
+//
+//  Created by Nadzeya Shpakouskaya on 03/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct RegistrationAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
